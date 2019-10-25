ff41d305c879062c8fa60d11aa8580b16fcd5ac6
vanesa888 <vanesa.bahalam@gmail.com>
2019-10-25 10:17:38 +0800

    setting admin LTe

---
+++ resources/views/template/head.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <link rel="stylesheet" href="{{asset('css/app.css')}}">
+   <meta name="csrf-token" content="{{csrf_token()}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
